6bc5d9941a517acc3513df98627d58d7b9b92514
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-05-31 11:39:51 +0200

    Updated default app.php to yse Postgres db

--- config/app.php
+++ config/app.php
@@ -210,7 +210,7 @@ return [
     'Datasources' => [
         'default' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
+            'driver' => 'Cake\Database\Driver\Postgres',
             'persistent' => false,
             'host' => 'localhost',
             /**
@@ -219,8 +219,8 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'nonstandard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
+            'username' => 'vagrant',
+            'password' => '',
             'database' => 'my_app',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
@@ -251,7 +251,7 @@ return [
          */
         'test' => [
             'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
+            'driver' => 'Cake\Database\Driver\Postgres',
             'persistent' => false,
             'host' => 'localhost',
             //'port' => 'nonstandard_port_number',
