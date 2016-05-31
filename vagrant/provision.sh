879f0abe187d68ac082629bd4873b144cb5a778e
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-05-31 12:54:50 +0200

    Comments, minor changes

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+#-------------------------------------------------------------------------------
+# Vagrant configuration file, highly based on
+#   https://github.com/tektoh/heroku-cakephp3-app
+#   https://github.com/alayek/zeus
+#   and some strings found on puphpet boxes (http://puphpet.com)
+#
+# Author: Manuel Tancoigne
+# Original authors:
+#   - Shinichi Segawa (https://github.com/tektoh/),
+#   - Arijit Layek (https://github.com/alayek/)
+#
+# This box will create a minimal virtual box (ubuntu trusty-x64) with minimal
+# packages required to run an apache2 webserver with php5 and postgreSQL.
+#
+# CakePHP is a slightly modified version of the official cakePHP 3.2, with minor
+# changes to make Heroku deployment a piece of cake.
+#
+# All credit goes to Shinichi Segawa (tektoh) for his implementation, I only
+# updated his work to CakePHP 3.2.
+#-------------------------------------------------------------------------------
+
 # Installation settings for a PHP box with CakePHP installed
 PROJECT="my_project" # we would want a name passed to it via te first argument, $1
 DB="my_app" # the name of postgreSQL DB we need to provision, maybe $2
@@ -13,7 +34,7 @@ echo "---------------------------------------------"
 echo "Running vagrant provisioning"
 echo "---------------------------------------------"
 
-# install heroku toolbelt
+# Install heroku toolbelt
 echo "- Adding heroku toolbelt to packages sources "
 # These shell script snippets are directly taken from heroku installation script
 echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
@@ -36,10 +57,13 @@ echo "---------------------------------------------"
 #  - ruby : needed for heroku toolbelt
 #  - curl : needed to download things
 apt-get install -y --no-install-recommends heroku-toolbelt ruby dos2unix man curl
+
 # Installing the PHP dev stack
-apt-get install -y --no-install-recommends git apache2 postgresql postgresql-contrib php5 php5-intl php5-pgsql php5-mcrypt php5-sqlite php5-apcu php5-cli
-# install Heroku CLI
+apt-get install -y --no-install-recommends git apache2 postgresql postgresql-contrib phpunit php5 php5-intl php5-pgsql php5-mcrypt php5-sqlite php5-apcu php5-cli
+
+# Install Heroku CLI
 su - vagrant -c "heroku --version > /dev/null 2>&1"
+
 # Enabling PHP mcrypt
 php5enmod mcrypt
 
